f6712a5ac3454dae52e3002ceaf9f96a58f21a63
briknthewall <blbracher@gmail.com>
2018-11-29 09:33:49 -0500

    added ride and transport classes,potentitally fixed db issue

---
+++ Hopper.Data/Ride.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hopper.Data
+{
+    public class Ride
+    {
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+        public int DistanceLength { get; set; }
+        public DateTime DistanceTime { get; set; }
+        public DateTime RideDate { get; set; }
+    }
+}
---
+++ Hopper.Data/Transport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hopper.Data
+{
+    public enum AnimalType {Toad,Kangaroo,Rabbit,Grasshopper,Spider}
+
+    public class Transport
+    {
+        public int TransportId { get; set; }
+        public AnimalType TransportAnimal { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+    }
+}
